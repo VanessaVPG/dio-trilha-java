7c212a19c6da973c71bec49d51ccc13dcec96004
vanessavpg <vanessa.pg026@gmail.com>
2023-01-04 20:15:06 -0300

    "operadores lógicos"

---
+++ operadorees/src/Operadores.java
@@ -0,0 +1,61 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int i1 = 10;
+        int i2 = 20;
+        float f1 = 4.5f;
+        float f2 = 3.5f;
+        double d1 =  59.6d;
+        char c1 = 'x';
+        char c2 = 'y';
+        String s1 = "Fulano";
+        String s2 = "Fulano";
+        boolean b1 = true;
+        boolean b2 = false;
+
+        long l1 = 1597L;
+        long l2 = 8997L;
+        byte y1 = 1;
+        short h1 = 25;
+
+        System.out.println("i1 == i2 " + (i1==i2));
+        System.out.println("i1 != i2 " + (i1!=i2));
+        System.out.println("i1 > i2 " + (i1>i2));
+        System.out.println("i1 <= i2 " + (i1<=i2));
+
+        System.out.println("f1 == f2 " + (f1==f2));
+        System.out.println("f1 != f2 " + (f1!=f2));
+        System.out.println("f1 >= f2 " + (f1>=f2));
+        System.out.println("f1 < f2 " + (f1<f2));
+
+        System.out.println("c1 == c2 " + (c1==c2));
+        System.out.println("c1 != c2 " + (c1!=c2));
+        System.out.println("c1 >= c2 " + (c1>=c2));
+        System.out.println("c1 < c2 " + (c1<c2));
+
+        System.out.println("s1 == s2 " + (s1==s2));
+        System.out.println("s1 != s2 " + (s1!=s2));
+
+        System.out.println("b1 == b2 " + (b1==b2));
+        System.out.println("b1 != b2 " + (b1!=b2));
+
+        System.out.println("l1 == l2 " + (l1==l2));
+        System.out.println("l1 != l2 " + (l1==l2));
+        System.out.println("l1 > l2 " + (l1>l2));
+        System.out.println("l1 =< l2 " + (l1==l2));
+
+
+        System.out.println("d1 != h1 " + (d1!=h1));
+        System.out.println("d1 == h1 " + (d1==h1));
+        System.out.println("y1 != h1 " + (y1!=h1));
+        System.out.println("y1 > h1 " + (y1 > h1));
+
+
+
+
+
+
+
+
+
+    }
+}
